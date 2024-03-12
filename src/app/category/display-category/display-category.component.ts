915e313a4d4d6de5e18866d4d7ffa1881bde89b0
brachi-f <braf5866@gmail.com>
2024-03-13 01:36:59 +0200

    🥱😔😟😤☹😲

---
+++ src/app/category/display-category/display-category.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'display-category',
+  standalone: true,
+  imports: [],
+  templateUrl: './display-category.component.html',
+  styleUrl: './display-category.component.scss'
+})
+export class DisplayCategoryComponent {
+
+}
