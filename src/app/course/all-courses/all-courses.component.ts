fe2478689026968445fec89c0b0360549e846e36
brachi-f <braf5866@gmail.com>
2024-03-13 16:44:01 +0200

    all course

--- src/app/course/all-courses/all-courses.component.ts
+++ src/app/course/all-courses/all-courses.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CourseDetailsComponent } from '../course-details/course-details.component';
+import { Course } from '../../Entities/Course.model';
+import { CourseService } from '../course.service';
 
 @Component({
   selector: 'all-courses',
@@ -10,6 +12,22 @@ import { CourseDetailsComponent } from '../course-details/course-details.compone
   templateUrl: './all-courses.component.html',
   styleUrl: './all-courses.component.scss'
 })
-export class AllCoursesComponent {
+export class AllCoursesComponent implements OnInit{
+  
+  public courseList?:Course[];
+
+  ngOnInit(): void {
+    this._courseService.getListOfCourses().subscribe({
+      next: (res)=>{
+        this.courseList = res;
+      },
+      error: (err)=>{
+        console.error(err.error);
+      }
+    })
+  }
+
+  constructor(private _courseService : CourseService){}
+
 
 }
