f70028e35e8d9b5de63e6a08c7077bc46ab7f9b1
brachi-f <braf5866@gmail.com>
2024-03-14 18:41:54 +0200

    get courses by user

--- src/app/course/all-courses/all-courses.component.ts
+++ src/app/course/all-courses/all-courses.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { CourseDetailsComponent } from '../course-details/course-details.component';
 import { Course } from '../../Entities/Course.model';
 import { CourseService } from '../course.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'all-courses',
@@ -12,35 +13,37 @@ import { CourseService } from '../course.service';
   templateUrl: './all-courses.component.html',
   styleUrl: './all-courses.component.scss'
 })
-export class AllCoursesComponent implements OnInit{
-  
-  public courseList?:Course[];
+export class AllCoursesComponent implements OnInit {
 
-  userId?:number;
+  public courseList?: Course[];
+
+  userId?: number;
 
   ngOnInit(): void {
-    if(!this.userId)
-    this._courseService.getListOfCourses().subscribe({
-      next: (res)=>{
-        this.courseList = res;
-        console.log("res",res)
-      },
-      error: (err)=>{
-        console.error(err.error);
+    this._route.paramMap.subscribe(params => {
+      this.userId = Number(params.get('id'));
+      if(this.userId){
+        this._courseService.getCoursesByUser(this.userId).subscribe({
+          next:(res)=>{
+            this.courseList = res;
+          },
+          error:(err)=>{
+            console.error("error in getting user's courses",err)
+          }
+        })
+      }else{
+        this._courseService.getListOfCourses().subscribe({
+          next:(res)=>{
+            this.courseList = res;
+          },
+          error:(err)=>{
+            console.error("error in getting all the courses",err)
+          }
+        });
       }
-    })
-    else
-    this._courseService.getCoursesByUser(this.userId).subscribe({
-      next: (res)=>{
-        this.courseList = res;
-        console.log("res",res)
-      },
-      error: (err)=>{
-        console.error(err.error);
-      }
-  })
+    });
   }
 
-  constructor(private _courseService : CourseService){}
+  constructor(private _courseService: CourseService, private _route: ActivatedRoute) { }
 
 }
